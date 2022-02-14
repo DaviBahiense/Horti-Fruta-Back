339184385dbe64f4562a3629bf51b1a18b664e26
Thais <tfrancag@gmail.com>
2022-02-14 00:04:22 -0300

    Feature. Added get to user orders

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { placeOrder } from "../controllers/orderController.js";
+import { placeOrder, getOrders } from "../controllers/orderController.js";
 
 const orderRouter = Router();
 
 orderRouter.post("/order", placeOrder);
-
+orderRouter.get("/user/orders", getOrders);
 export default orderRouter;
